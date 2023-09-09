1b3398ed2939fd876a94081ffa3a31cc4f036e3f
cam5674 <103235007+cam5674@users.noreply.github.com>
2023-09-09 15:40:43 -0700

    Add files via upload

---
+++ Alien Project/game_stats.py
@@ -0,0 +1,13 @@
+
+
+class GameStats:
+    """Track stas for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize stats"""
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize stats that can change during the game."""
+        self.ships_left = self.settings.ship_limit
